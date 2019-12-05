e49b2d7dc0a1acda37d595289fe6fb67b2ef4253
Yevhenii <pro100elite@icloud.com>
2019-12-05 14:47:05 +0200

    Test for you

---
+++ WindowsForms4/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsForms4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(205, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверка";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(189, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 84);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 331);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(500, 120);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsForms4/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms4
+{
+    public partial class Form1 : Form
+    {
+
+        string fl1 = @"C:\Users\Pro100Elite\Desktop\kosty3.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rr(result("Проверка завершена", "Ответ"));
+        }
+
+        string Slovo(string str, string str2) // свойство
+        {
+            return $" Файл\n {str} {str2} существует";
+        }
+
+        bool _Bool(string line) // свойство
+        {
+            return File.Exists(line) ? true : false;
+        }
+
+        DialogResult result(string str, string str2) // свойство
+        {
+            return MessageBox.Show(str, str2, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        void Rr(DialogResult result) // метод
+        {
+            if (DialogResult.OK == result)
+            {
+                label1.Text = _Bool(fl1) ? Slovo($"{fl1}", null) : Slovo($"{fl1}", "не");
+            }
+        }
+    }
+}
